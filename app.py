09e784ac205b9a56d5aaff7404056043c94912c8
Tsabyh <tsabyhseif12@gmail.com>
2025-09-24 15:24:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,9 @@ def home():
     conn.close()
 
     return render_template("index.html", reviews=reviews)
-
-
-if __name__ == '__main__':
+    
+    if __name__ == '__main__':
     init_db()
-    app.run(host-'0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',debug=True)
+
+
